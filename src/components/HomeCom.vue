13fa646f4a4a710f69f5070f1b68185902e650bb
Willy-WZW <joystick449@.gmail.com>
2024-07-17 13:40:05 +0800

    btnAnime

--- src/components/HomeCom.vue
+++ src/components/HomeCom.vue
@@ -20,7 +20,16 @@ export default {
       >&mdash;&mdash;&mdash;&mdash;&mdash;TAIWAN
       CHAPTER&mdash;&mdash;&mdash;&mdash;&mdash;</span
     >
-    <RouterLink to="/air"><button>Let's Go<i class="fa-solid fa-caret-right"style="position: relative; right: -5px"></i></button></RouterLink>
+
+    <RouterLink to="/air" class="btnAnime">
+      <button>
+        Let's Go
+        <i
+          class="fa-solid fa-caret-right"
+          style="position: relative; right: -5px"
+        ></i>
+      </button>
+    </RouterLink>
   </div>
 </template>
 
@@ -28,6 +37,7 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Bona+Nova+SC:ital,wght@0,400;0,700;1,400&family=Edu+AU+VIC+WA+NT+Hand:wght@400..700&family=Playwrite+CU:wght@100..400&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Bona+Nova+SC:ital,wght@0,400;0,700;1,400&family=Edu+AU+VIC+WA+NT+Hand:wght@400..700&family=Playwrite+CU:wght@100..400&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto+Slab:wght@100..900&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Bangers&family=Bona+Nova+SC:ital,wght@0,400;0,700;1,400&family=Edu+AU+VIC+WA+NT+Hand:wght@400..700&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Playwrite+CU:wght@100..400&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto+Slab:wght@100..900&display=swap");
+
 .homeimg {
   width: 99dvw;
   height: 100dvh;
@@ -59,6 +69,7 @@ export default {
     z-index: 2;
     text-shadow: 2px 2px 2px #000;
   }
+
   span {
     font-family: "Bona Nova SC", serif;
     position: fixed;
@@ -69,44 +80,50 @@ export default {
     z-index: 2;
     text-shadow: 2px 2px 2px #000;
   }
-  button {
-    width: 120px;
-    border-radius: 3px;
-    font-family: "Kanit", sans-serif;
-    cursor: pointer;
-    position: absolute;
-    top: 68%;
-    left: 65.1%;
-    font-size: 15px;
-    z-index: 3;
-    border: 2px solid rgba(255, 255, 255, 0.8);
-    letter-spacing: 2px;
-    background-color: rgba(255, 255, 255, 0);
-    font-weight: 200;
-    color: white;
-    text-shadow: 2px 2px 2px #000;
-    box-shadow: 1px 1px 7px 1px #000;
-    overflow: hidden;
 
+  .btnAnime {
     &::before {
       content: "";
       position: absolute;
-      top: 0;
-      left: -120px;
-      width: 120px;
+      top: 68%;
+      left: 65.1%;
+      width: 0px;
       height: 26px;
-      background-color: white;
+      background-color: rgb(255, 255, 255);
       z-index: 2;
-      transition: left 0.5s;
+      transition: all 0.8s;
       opacity: 0;
-
-      &:hover::before{
-        left: 100% ;
+    }
+    &:hover {
+      &::before {
+        width: 120px;
+        left: 65.1%;
         opacity: 1;
       }
-      // &:hover{
-      //   color: #000
-      // }
+    }
+    button {
+      width: 120px;
+      border-radius: 3px;
+      font-family: "Kanit", sans-serif;
+      cursor: pointer;
+      position: absolute;
+      top: 68%;
+      left: 65.1%;
+      font-size: 15px;
+      z-index: 3;
+      border: 2px solid rgba(255, 255, 255, 0.8);
+      letter-spacing: 2px;
+      background-color: rgba(255, 255, 255, 0);
+      font-weight: 200;
+      color: white;
+      box-shadow: 1px 1px 7px 1px #000;
+      overflow: hidden;
+
+      &:hover {
+        color: #000;
+        background: transparent;
+        text-shadow: 1px 1px 2px #000;
+      }
     }
   }
 }
